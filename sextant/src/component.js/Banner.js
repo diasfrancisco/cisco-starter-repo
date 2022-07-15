e4b20401b64a334bb4e701cf8ab48268664197ac
CiscoUbuntu <diasfrancisco7798@yahoo.co.uk>
2022-07-15 22:05:19 +0100

    added banner and exhibit components to the project and styled them

---
+++ sextant/src/component.js/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <h1>
+            Sextant
+        </h1>
+    </div>
+  )
+}
+
+export default Banner
---
+++ sextant/src/component.js/Exhibit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Exhibit = () => {
+  return (
+    <div className='container'>
+        <h2>
+            Menu
+        </h2>
+    </div>
+  )
+}
+
+export default Exhibit
